8ed74b39307643ef92bfb7c5151af8d41f442ad2
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-09 14:59:31 -0600

    Addition

---
+++ 1-D/code-examples/shockless_total_pressure_ratios.m
@@ -0,0 +1,14 @@
+syms x
+g = 1.4; 
+f1 = 1; 
+f2 = 1/x*((2/(g+1))*(1 + (g-1)/2*x^2))^((g+1)/(2*g-2));
+f3 = (1 + g)/(1 + g*x^2)*((2/(g+1))*(1 + (g-1)/2*x^2))^(g/(g-1));
+fplot(f1,[1 5],'Linewidth',1,'color','#D95319');
+hold on 
+fplot(f2,[1 5],'Linewidth',1,'color','red');
+hold on 
+fplot(f3,[1 5],'Linewidth',1,'color','magenta');
+xlabel('Mach Number');
+ylabel('p_{o} / p_{o}*');
+title('Sonic Total Pressure Ratios');
+legend('Isentropic','Adiabatic','Reversible Heat Transfer');
