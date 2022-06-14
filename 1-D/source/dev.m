742f873fcbf32c8b1764fbbb7bcbf90f09dc2e21
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-14 12:47:19 -0600

    shockless classes

--- 1-D/source/dev.m
+++ 1-D/source/dev.m
@@ -1,4 +1,1 @@
-normal_shock.plot(1.3,[1 5],'p','d','tp');
-normal_shock.plot(1.3,[1 6],'p','d','tp');
-%normal_shock.plot(1.3,[-1 5],'p','d','tp');
-normal_shock.argCheck(1.3,[-1 5],'p','d','tp')
+shockless.plot(1.1,[1 4],'p','tp');
