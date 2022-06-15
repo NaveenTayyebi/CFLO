f457b7f94fb46be88a1a02f797f012c18f548dc4
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-15 11:10:41 -0600

    sonic_ref_calc and arg_check additions

--- 1-D/source/dev.m
+++ 1-D/source/dev.m
@@ -1,2 +1,26 @@
-%shockless.plot(1.4,[1 5],'p','T','d','tp');
-normal_shock.plot(1.4,[1 5],'T');
+%shockless.plot(-1.2,[1 5],'d');
+%shockless.plot(0,[1 5],'d');
+%shockless.plot(0.00000000000001,[1 5],'d');
+%shockless.plot(-0.00000000000000001,[1 5],'d');
+%shockless.plot(Inf,[1 5],'d');
+%shockless.plot(-Inf,[1 5],'d');
+%shockless.plot([1 2],[1 5],'d');
+%shockless.plot([-1 -2],[1 5],'d');
+%shockless.plot([1 ; 2],[1 5],'d');
+%shockless.plot([-1 ; -2],[1 5],'d');
+%shockless.plot([1 1 1 ; 1 1 1],[1 5],'d');
+%shockless.plot([1 ;1 ; 1],[1 5],'d');
+%shockless.plot(1i,[1 5],'d');
+%shockless.plot([1i 2i],[1 5],'d');
+%shockless.plot([-1i -2i],[1 5],'d');
+%shockless.plot([1i ;2i],[1 5],'d');
+%shockless.plot([-1i ; -2i],[1 5],'d');
+%shockless.plot([1i 2],[1 5],'d');
+%shockless.plot([-1i -2],[1 5],'d');
+%shockless.plot('a',[1 5],'d');
+%shockless.plot("a",[1 5],'d');
+%shockless.plot('ababab',[1 5],'d');
+%shockless.plot(["a" "b"],[1 5],'d');
+%shockless.plot({1.2},[1 5],'d');
+%shockless.plot(@(x) x.^2,[1 5],'d');
+shockless.sonic_ref_calc(1.4,[2 2],'p','is')
