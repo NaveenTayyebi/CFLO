755733b15fccbbaaa0ff0484152ff0072d659549
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-16 13:13:52 -0700

    Testing

--- 1-D/source/dev.m
+++ 1-D/source/dev.m
@@ -23,6 +23,47 @@
 %shockless.plot(["a" "b"],[1 5],'d');
 %shockless.plot({1.2},[1 5],'d');
 %shockless.plot(@(x) x.^2,[1 5],'d');
+%shockless.arg_2check(-1.2,2,'p');
+%shockless.arg_2check(1.2i,2,'p');
+%shockless.arg_2check([-1.2 -1.2],2,'p');
+%shockless.arg_2check([1.2 1.2],2,'p');
+%shockless.arg_2check([1.2 ; 1.2],2,'p');
+%shockless.arg_2check([0.2 ; 0.2],2,'p');
+%shockless.arg_2check([0.2 ; 0.2 ; 0.2],2,'p');
+%shockless.arg_2check(1.2i,2,'p');
+%shockless.arg_2check(1.00000,2,'p');
+%shockless.arg_2check(1.1,-0.00000000001,'p');
+%shockless.arg_2check(1.2,[-0.00001 0.0001],'p');
+%shockless.arg_2check(1.2,[0.00001 -0.0001],'p');
+%shockless.arg_2check(1.2,[2 2],'p');
+a.a.a = 'a'; 
+mach = {[0.0000001 0.000001],[0.00001 ; 0.00001],[0.00001 0.00001...
+    ; 0.000001 0.00001],-0.00001,0.00001i,2+i, [2 + 2i 2 + 2i],...
+    @(x) x, {0.0001}, 0:0.1:1,'a',"a",'ababab',"abababa",['a' 'a'],...
+    ['b' 1],[Inf 0],-Inf,a.a.a,0.999};
+specHeatRatio = {[0.0000001 0.000001],[0.00001 ; 0.00001],[0.00001 0.00001...
+    ; 0.000001 0.00001],-0.00001,0.00001i,2+2i, [2 + 2i 2 + 2i],...
+    @(x) x, {0.0001}, 0:0.1:1,'a',"a",'ababab',"abababa",['a' 'a'],...
+    ['b' 1],[Inf 0],-Inf,a.a.a,0.999};
+abbreviation = {'p','d','T','tp','asda','asdas','juEU',1212,2+2i,2}; 
+disp(length(mach))
+for i = 1:length(mach) 
+    try shockless.arg_2check(1.2,mach{i},'p');
+    catch 
+        disp(i)
+    end 
+end 
+disp(" ")
+machRange = {[2 3],[0.00001 ; 0.00001],[0.00001 0.00001...
+    ; 0.000001 0.00001],-0.00001,0.00001i,2+i, [2 + 2i 2 + 2i],...
+    @(x) x, {0.0001}, 0:0.1:1,'a',"a",'ababab',"abababa",['a' 'a'],...
+    ['b' 1],[Inf 0],-Inf,a.a.a,0.999};
+for i = 1:length(machRange) 
+    try shockless.arg_check(1.2,machRange,'p');
+    catch 
+        disp(i)
+    end 
+end 
 
 
 
