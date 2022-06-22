e9bba427d5b232c9d2f5e7fe97b489076c57ee49
Naveen Tayyebi <68218266+NaveenTayyebi@users.noreply.github.com>
2022-06-22 14:46:57 -0700

    Delete mylanglexer.py

--- docs/mylanglexer.py
+++
@@ -1,9 +0,0 @@
-from pygments.lexers import get_lexer_by_name  # refer LEXERS
-from pygments.lexers._mapping import LEXERS
-from pygments.lexers.python import PythonLexer
-
-
-def setup(app):
-    # choose one, both ok
-    app.add_lexer('my_lang', get_lexer_by_name('matlab'))
-    # app.add_lexer('my_lang', PythonLexer)
