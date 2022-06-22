5f7a4ed7174dda32c33f88226c2eb899abe18eb7
Naveen Tayyebi <68218266+NaveenTayyebi@users.noreply.github.com>
2022-06-21 23:16:13 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Compressible Flow'
+project = 'CFLO'
 copyright = '2022, Naveen Tayyebi'
 author = 'Naveen Tayyebi'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
