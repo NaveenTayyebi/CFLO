3246bbd6077af799b85bd48fe51e75f661f928af
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-25 16:39:29 -0700

    bug fixes

--- 1-D/source/dev.m
+++ 1-D/source/dev.m
@@ -1,5 +1,5 @@
-%shockless.total_ref_plot(1.4,[0 5],'p','T');
-%shockless.total_ref_plot(1.4,[0 5],'p','d'); 
+%shockless.total_ref_calc(1.4,1,'T')
+%shockless.sonic_ref_plot(1.4,[0 5],'p','d'); 
 shockless.sonic_ref_plot(1.4,[1 10],'T')
 
 
