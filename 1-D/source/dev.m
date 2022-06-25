43585ba416a6cc8583700a58939b0d8009c01046
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-25 00:50:04 -0700

    total ref plot and calc additions, and bug fixes

--- 1-D/source/dev.m
+++ 1-D/source/dev.m
@@ -1,6 +1,6 @@
-shockless.sonic_ref_plot(1.4,[1 5],'p','d')
-
-normal_shock.upstream_ref_calc(1)
-
+shockless.total_ref_plot(1.4,[1 5],'p','T');
+%shockless.total_ref_calc(1.4,[-2 2],'p')
+shockless.sonic_ref_plot(1.4,[-1 5],'p','d');
+shockless.sonic_ref_calc(1.4,3,'p','rvht')
 
 
