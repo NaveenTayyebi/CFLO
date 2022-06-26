0af38ddfc611f8b32fa829db8122af9525c78fa5
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-25 20:47:52 -0700

    1-d parameter labels changed

--- 1-D/source/dev.m
+++ 1-D/source/dev.m
@@ -1,6 +1,5 @@
 %shockless.sonic_ref_calc(1.4,4,'tp','ray')
 
 
-normal_shock.upstream_ref_calc(1.4,3,'p')
-normal_shock.upstream_ref_plot(1.4,[1 4],'p');
+normal_shock.upstream_ref_plot(1.4,[1 4],'p','d');
 
