9c377a892ad2231b0f22ad390f83d2e0639d12ea
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-25 17:44:03 -0700

    change ad to fan and rvht to ray' :q :qa!

--- 1-D/source/dev.m
+++ 1-D/source/dev.m
@@ -1,8 +1,5 @@
-%shockless.total_ref_calc(1.4,1,'T')
-%shockless.sonic_ref_plot(1.4,[0 5],'p','d'); 
-%shockless.sonic_ref_plot(1.4,[0 5],'A')
-shockless.sonic_ref_plot(1.4,[0 4],'A');
-shockless.sonic_ref_calc(1,4,'A','ad')
+shockless.sonic_ref_calc(1.4,4,'tp','ray')
+
 
 
 
