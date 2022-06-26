e785a1716a8c01716ed5542f78a838ab52c58797
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-25 20:38:00 -0700

    rename variables specHeatRatio to spec_heat_ratio, and so on

--- 1-D/source/dev.m
+++ 1-D/source/dev.m
@@ -1,5 +1,6 @@
-shockless.sonic_ref_calc(1.4,4,'tp','ray')
+%shockless.sonic_ref_calc(1.4,4,'tp','ray')
 
 
-
+normal_shock.upstream_ref_calc(1.4,3,'p')
+normal_shock.upstream_ref_plot(1.4,[1 4],'p');
 
