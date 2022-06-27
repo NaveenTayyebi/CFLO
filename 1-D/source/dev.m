f45072487fe6abf09dac325bf9949a5110cf9717
NaveenTayyebi <naveentayyebi@gmail.com>
2022-06-27 09:24:47 -0700

    Plot done fanno_mach

--- 1-D/source/dev.m
+++ 1-D/source/dev.m
@@ -2,7 +2,7 @@
 %shockless.sonic_ref_plot(1.4,[0.2 2],'p');
 
 
-shockless.fanno_mach(1.1,3,0.02,'calc','sl','sl');
+shockless.fanno_mach(1.1,[0.2 2],3,0.02,'plot','sl');
 
 %shockless.sonic_ref_plot(1.4,[1 3],'p')
 
